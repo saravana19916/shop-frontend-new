af5a79f55e11f72935cf3ff9f5eed12058cb98e5
vsaravanakumar <saravanan@kuruntham.com>
2025-04-04 21:41:53 +0530

    commit on 20250404

--- src/shared/SocialsList1.tsx
+++ src/shared/SocialsList1.tsx
@@ -49,6 +49,7 @@ const SocialsList1: FC<SocialsList1Props> = ({
       <>
         <a
           href={item.href}
+          target="_blank"
           className="flex items-center text-2xl text-neutral-700 hover:text-black dark:text-neutral-300 dark:hover:text-white leading-none space-x-2 group"
           key={index}
         >
@@ -66,22 +67,23 @@ const SocialsList1: FC<SocialsList1Props> = ({
       {socials.map(renderItem)}
       <a
         style={{
-          border:
-            "2px solid rgba(var(--c-neutral-700), var(--tw-text-opacity))",
           borderRadius: "5px",
         }}
         href="https://twitter.com/TixBox"
-        className="flex items-center text-2xl text-neutral-700 hover:text-black dark:text-neutral-300 dark:hover:text-white leading-none space-x-2 group"
+        target="_blank"
+        className="flex items-center text-2xl text-neutral-700 hover:text-black border border-1 border-neutral-700 dark:border-neutral-400 dark:text-neutral-300 dark:hover:text-white leading-none space-x-2 group"
       >
         <svg
           style={{ padding: "2px" }}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 512 512"
-          width="17px"
-          height="17px"
+          width="14px"
+          height="14px"
+          className="fill-black dark:fill-white"
         >
           <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
         </svg>
+
       </a>
     </div>
   );
